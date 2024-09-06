9581fd0c0e2ab1b334fa119d214e2f67a81b9941
Athul KM <69643614+AthulKM@users.noreply.github.com>
2024-09-06 10:32:07 +0530

    CRED added for category management

--- controllers/category.js
+++ controllers/category.js
@@ -1,4 +1,5 @@
 
+import upload from '../middlewares/uploadImage.js';
 import Category from '../models/category.js';
 
 export const addNewCategory = async (req, res) => {
@@ -45,11 +46,122 @@ export const addNewCategory = async (req, res) => {
 
 };
 
-// const editCategory = async (req, res) => {
-//     const category = req.body.category;
-//     const checkIfExists = Category.findOne(category)
+export const getAllCategories = async (req, res) => {
+    try {
+        const categories = await Category.find();
+        res.status(200).json({
+            Message: "Categories have been fetched successfully",
+            data: categories,
+            status: "Success",
+            error: false
+        });
 
-//     if (!checkIfExists) {
-//         res.status(404).
-//     }
-// }
+    } catch (error) {
+        res.status(500).json({
+            Message: "Failed to retrieve data",
+            status: "Failed",
+            error: true
+        })
+    }
+};
+
+export const getCategoryById = async (req, res) => {
+    const categoryId = req.params.id;
+
+    try {
+        const category = await Category.findById(categoryId);
+
+        if (!category) {
+            return res.status(404).json({
+                Message: "Category not found",
+                status: "Failed",
+                error: true
+            });
+
+        }
+        res.status(200).json({
+            Message: "Category retrieved successfully",
+            status: "Success",
+            error: false
+        });
+
+    } catch (error) {
+        res.status(500).json({
+            Message: "Failed to retrieve",
+            status: "Failed",
+            error: true
+        });
+    }
+};
+
+export const updateCategory = async (req, res) => {
+    upload(req, res, async (err) => {
+        if (err) {
+            return res.status(400).json({
+                Message: "Error uploading file",
+                status: "Failed",
+                error: true
+            });
+        }
+        const { name } = req.body;
+        const categoryId = req.params.id;
+        const updatedData = {
+            name: name
+        };
+        if (req.file) {
+            updatedData.image = req.file.path;
+        }
+        try {
+            const updatedCategory = await Category.findByIdAndUpdate(
+                categoryId,
+                updatedData,
+                { new: true }
+            );
+            if (!updatedCategory) {
+                return res.status(404).json({
+                    Message: "Category not found",
+                    status: "Failed",
+                    error: true
+                });
+            }
+            res.status(200).json({
+                message: "Category updated successfully",
+                category: updatedCategory
+            });
+        } catch (error) {
+            res.status(500).json({
+                Message: "Failed to update",
+                status: "Failed",
+                error: true
+            });
+        }
+    });
+};
+
+export const deleteCategory = async (req, res) => {
+    const categoryId = req.params.id;
+
+    try {
+        const deletedCategory = await Category.findByIdAndDelete(categoryId);
+
+        if (!deletedCategory) {
+            return res.status(404).json({
+                Message: "Category not found",
+                status: "Failed",
+                error: true
+            });
+        }
+        res.status(200).json({
+            Message: "Category deleted successfully",
+            status: "Success",
+            error: false,
+            data: deletedCategory
+        });
+    } catch (error) {
+        res.status(500).json({
+            Message: "Failed to delete category",
+            status: "Failed",
+            error: True
+        });
+    }
+};
--- routes/category.js
+++ routes/category.js
@@ -1,10 +1,15 @@
 import Express from 'express';
-import { addNewCategory } from "../controllers/category.js";
+import { addNewCategory, deleteCategory, getAllCategories, getCategoryById, updateCategory } from "../controllers/category.js";
 import upload from '../middlewares/uploadImage.js';
 
 
 const router = Express.Router();
 
 router.post('/new', upload.single('image'), addNewCategory);
+
+router.get('/all', getAllCategories);
+router.get('/:id', getCategoryById);
+router.put('/:id', updateCategory);
+router.delete('/:id', deleteCategory);
 
 export default router;
